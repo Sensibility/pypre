b474c80f4976f2b2d5bb94b84c72cd908ccc10fc
ocket8888 <ocket8888@gmail.com>
2018-02-23 10:19:29 -0700

    License updates; copyright and warranty sections now properly located in each source file.

--- pypre/__init__.py
+++ pypre/__init__.py
@@ -234,6 +234,29 @@ Some caveats and disclaimers:
 
 """
 
+"""
+Copyright 2018, Github (https://github.com/) user ocket8888 (https://github.com/ocket8888),
+                Github organization Sensibility (https://github.com/Sensibility)
+"""
+
+"""
+This file is part of pypre.
+
+    pypre is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pypre is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pypre.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 __author__ = "ocket8888"
 __version__ = "0.2.0"
 
@@ -247,7 +270,7 @@ def main() -> int:
 	"""
 
 	parser = argparse.ArgumentParser(description='A preprocessor for Python.',
-	                                 epilog='Uses somewhat C-like syntax. For full usage help, see https://github.com/Sensibility/pypre')
+	epilog='Uses somewhat C-like syntax. For full usage help see https://github.com/Sensibility/pypre')
 
 	parser.add_argument("-i",
 	                    "--input",
